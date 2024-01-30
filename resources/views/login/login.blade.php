7fecd25692f8cec0c6946b124d5bb56c34b57d0d
ArfandaAgung <arfandaagung@gmail.com>
2024-01-30 15:03:14 +0800

    login page baru

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -6,15 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ '' }}css/login.css" />
-    <title>Sign in & Sign up Form</title>
+    <title>ALUMNI SMKIT IQIS</title>
 </head>
 
 <body>
     <div class="container">
         <div class="forms-container">
             <div class="signin-signup">
-                <!-- Form 1 Sign In -->
+                <!-- Form 1 -->
                 <form action="{{ route('siswa.index') }}" method="GET" class="sign-in-form">
+                    <img src="{{ '' }} img/smk.png" alt="" class="logos">
                     <h2 class="title">Sign in</h2>
                     <div class="input-field">
                         <i class="fas fa-user"></i>
@@ -26,25 +27,26 @@
                     </div>
                     <input type="submit" value="Login" class="btn solid" />
 
-                    <p class="social-text">Or Sign in with social platforms</p>
+                    <p class="social-text">Our Social Media</p>
                     <div class="social-media">
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-facebook-f"></i>
+                        <a href="https://www.instagram.com/smkitibnulqayyim/" class="social-icon">
+                            <i class="fab fa-instagram"></i>
                         </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-twitter"></i>
+                        <a href="https://www.youtube.com/@IQISTV" class="social-icon">
+                            <i class="fab fa-youtube"></i>
                         </a>
-                        <a href="#" class="social-icon">
+                        <a href="https://www.smkit.iqis.sch.id/" class="social-icon">
                             <i class="fab fa-google"></i>
                         </a>
                         <a href="#" class="social-icon">
-                            <i class="fab fa-linkedin-in"></i>
+                            <i class="fab fa-whatsapp"></i>
                         </a>
                     </div>
                 </form>
 
-                <!-- Form 2  Sign Up-->
+                <!-- Form 2 -->
                 <form action="#" class="sign-up-form">
+                <img src="{{ '' }} img/smk.png" alt="" class="logos">
                     <h2 class="title">Sign up</h2>
                     <div class="input-field">
                         <i class="fas fa-user"></i>
@@ -83,8 +85,7 @@
                 <div class="content">
                     <h3>New here ?</h3>
                     <p>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
-                        ex ratione. Aliquid!
+                    If you're new here, we recommend registering an account first.
                     </p>
                     <button class="btn transparent" id="sign-up-btn">
                         Sign up
@@ -96,8 +97,7 @@
                 <div class="content">
                     <h3>One of us ?</h3>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-                        laboriosam ad deleniti.
+                    If you are one of us, please log in from here.
                     </p>
                     <button class="btn transparent" id="sign-in-btn">
                         Sign in
