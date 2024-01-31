2d7c094ed2daee1995737bf30a9a1dc7b7dbf57a
AndiRasyihan <rasyihanjawahir@iqis.sch.id>
2024-01-31 21:01:25 +0800

    otw

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -14,18 +14,19 @@
         <div class="forms-container">
             <div class="signin-signup">
                 <!-- Form 1 -->
-                <form action="{{ route('siswa.index') }}" method="GET" class="sign-in-form">
+                <form action="/login" method="POST" class="sign-in-form">
+                    @csrf
                     <img src="{{ '' }} img/SMKIT IQIS Logo.png" alt="" class="logos">
                     <h2 class="title">Sign in</h2>
                     <div class="input-field">
                         <i class="fas fa-user"></i>
-                        <input type="text" placeholder="Username" />
+                        <input type="text" placeholder="Email" name="email" />
                     </div>
                     <div class="input-field">
                         <i class="fas fa-lock"></i>
-                        <input type="password" placeholder="Password" />
+                        <input type="password" placeholder="Password" name="password" />
                     </div>
-                    <input type="submit" value="Login" class="btn solid" />
+                    <input type="submit" class="btn solid" />
 
                     <p class="social-text">Our Social Media</p>
                     <div class="social-media">
@@ -43,73 +44,38 @@
                         </a>
                     </div>
                 </form>
-
-                <!-- Form 2 -->
-                <form action="#" class="sign-up-form">
-                <!-- <img src="{{ '' }} img/SMKIT IQIS Logo.png" alt="" class="logos"> -->
-                <img src="{{ '' }} img/SMKIT IQIS Logo.png" alt="" class="logos">
-                    <h2 class="title">Sign up</h2>
-                    <div class="input-field">
-                        <i class="fas fa-user"></i>
-                        <input type="text" placeholder="Username" />
-                    </div>
-                    <div class="input-field">
-                        <i class="fas fa-envelope"></i>
-                        <input type="email" placeholder="Email" />
-                    </div>
-                    <div class="input-field">
-                        <i class="fas fa-lock"></i>
-                        <input type="password" placeholder="Password" />
-                    </div>
-                    <input type="submit" class="btn" value="Sign up" />
-                    <p class="social-text">Or Sign up with social platforms</p>
-                    <div class="social-media">
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-facebook-f"></i>
-                        </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-twitter"></i>
-                        </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-google"></i>
-                        </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-linkedin-in"></i>
-                        </a>
-                    </div>
-                </form>
             </div>
         </div>
-
+        
         <div class="panels-container">
             <div class="panel left-panel">
                 <div class="content">
                     <h3>New here ?</h3>
                     <p>
-                    If you're new here, we recommend registering an account first.
+                        If you're new here, we recommend registering an account first.
                     </p>
                     <button class="btn transparent" id="sign-up-btn">
-                        Sign up
+                        <a href="/register">Sign up</a>
                     </button>
                 </div>
                 <img src="img/log.svg" class="image" alt="" />
             </div>
-            <div class="panel right-panel">
-                <div class="content">
-                    <h3>One of us ?</h3>
-                    <p>
-                    If you are one of us, please log in from here.
-                    </p>
-                    <button class="btn transparent" id="sign-in-btn">
-                        Sign in
-                    </button>
-                </div>
-                <img src="img/register.svg" class="image" alt="" />
-            </div>
+            <!-- <div class="panel right-panel"> -->
+                <!-- <div class="content"> -->
+                    <!-- <h3>One of us ?</h3> -->
+                    <!-- <p> -->
+                        <!-- If you are one of us, please log in from here. -->
+                    <!-- </p> -->
+                    <!-- <button class="btn transparent" id="sign-in-btn"> -->
+                        <!-- Sign in -->
+                    <!-- </button> -->
+                <!-- </div> -->
+                <!-- <img src="img/register.svg" class="image" alt="" /> -->
+            <!-- </div> -->
         </div>
     </div>
 
-    <script src="{{ '' }}js/login.js"></script>
+    <!-- <script src="{{ '' }}js/login.js"></script> -->
 </body>
 
 </html>
